692d1be52f6852cba4c4c7b5ce7fbcdc3b5b44f8
PinhoOn <pinhoonjob@gmail.com>
2021-05-20 13:41:37 -0300

    full styles for header

--- docs/main.54166fbf.js
+++ docs/main.54166fbf.js
@@ -189,7 +189,7 @@ var reloadCSS = require('_css_loader');
 
 module.hot.dispose(reloadCSS);
 module.hot.accept(reloadCSS);
-},{"/Users/matheusagcosta/Documents/nomadfy.github.io/src/assets/bg.png":[["bg.19f8c1d4.png","assets/bg.png"],"assets/bg.png"],"_css_loader":"../node_modules/parcel-bundler/src/builtins/css-loader.js"}],"../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+},{"/home/pinho/projects/nomadfy.github.io/src/assets/bg.png":[["bg.19f8c1d4.png","assets/bg.png"],"assets/bg.png"],"_css_loader":"../node_modules/parcel-bundler/src/builtins/css-loader.js"}],"../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
@@ -217,7 +217,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "52499" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "35939" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
