ff6d8ccca16502ab29a5a84f7286ba5555c02e56
PinhoOn <pinhoonjob@gmail.com>
2021-05-28 16:05:21 -0300

    layouts for review

--- docs/src.e31bb0bc.js
+++ docs/src.e31bb0bc.js
@@ -190,8 +190,6 @@ _explore.guide.forEach(function (item) {
   var destination = buildDestination(item);
   containerGuide.innerHTML = containerGuide.innerHTML + guide;
 });
-
-var results = document.currentScript;
 },{"./data/explore.json":"data/explore.json"}],"../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
@@ -220,7 +218,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "42165" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "46733" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
--- src/results.js
+++ src/results.js
@@ -1,4 +1,4 @@
-import { dropDownOptions } from "./js/dropdown.js"
+import { dropDownOptions, dropDownEdit, dropDownDetails } from "./js/dropdown.js"
 
 document.querySelector('#optionsButton').addEventListener('click', dropDownOptions);
 
@@ -10,3 +10,26 @@ window.addEventListener('click', function(event) {
     }
   }
 })
+
+document.querySelector('#editButton').addEventListener('click', dropDownEdit);
+
+window.addEventListener('click', function(event) {
+  if (!event.target.matches('#editButton')) {
+    var visible = document.getElementById('editDropdown');
+    if (visible.classList.contains('is-visible')) {
+      visible.classList.remove('is-visible');
+    }
+  }
+})
+
+document.querySelector('#detailsButton').addEventListener('click', dropDownDetails);
+
+window.addEventListener('click', function(event) {
+  if (!event.target.matches('#detailsButton')) {
+    var visible = document.getElementById('detailsDropdown');
+    if (visible.classList.contains('is-visible')) {
+      visible.classList.remove('is-visible');
+    }
+  }
+})
+
